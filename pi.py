3c7e3ffe470199c95661c069b876a983e158bba7
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-03-31 13:43:22 +0900

    Python入門　回答

--- pi.py
+++ pi.py
@@ -5,3 +5,10 @@ text = """
 
 # TODO
 
+text = "How I want a drink alcoholic of course after the heavy chapters involving\
+        quantum mechanics All of thy geometry Herr Planck is fairly hard"
+
+text1 = list(map(len,(text.split())))
+
+for i in text1:
+    print(str(i), end = "")

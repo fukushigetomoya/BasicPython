16383da40f765123ea0147abecd11368d70dc78f
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-04-01 11:35:02 +0900

    control_syntax

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,18 @@
+
 from math import sin
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
+
+import math
+
+
+def f(x):
+    return sin(x)
+
+N = 100
+a = 0
+b = math.pi / 2
+h = (b -  a) / N
+S = 0
+for i in range(N):
+    S += h / 2 * (f(a) + f(a + h))
+
+print(S)
